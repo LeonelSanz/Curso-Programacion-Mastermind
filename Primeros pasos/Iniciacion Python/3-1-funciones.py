1095baeccc8c2ba9b95478719936f6cc04aa803b
Leonel <leonelsanz15@gmail.com>
2023-12-02 19:36:55 -0300

    part 3 1-6

---
+++ Primeros pasos/Iniciacion Python/3-1-funciones.py
@@ -0,0 +1,14 @@
+
+def saludo_sectario(nombre):
+    print("Hola {}".format(nombre[::-1]))
+
+
+# saludo_sectario("Nate")
+# saludo_sectario("Leo")
+
+
+def potencia(num):
+    potencia_num = num * num
+    return potencia_num
+
+print(potencia(10))
